d2e896720bc373836b1c806a1956f6972ee346cf
sachin kudtarkar <sachinkudtarkar07@gmail.com>
2016-06-14 14:09:57 +0530

    new mudule added

--- modules/elastic/controllers/DefaultController.php
+++ modules/elastic/controllers/DefaultController.php
@@ -4,7 +4,9 @@ namespace app\modules\elastic\controllers;
 
 use yii\web\Controller;
 use app\modules\Elastic\models\Logs;
-
+use yii\elasticsearch;
+use \Elasticsearch\Transport;
+//require 'app/vendor/autoload.php';
 /**
  * Default controller for the `elastic` module
  */
@@ -16,16 +18,58 @@ class DefaultController extends Controller
      */
     public function actionIndex()
     {
-       // require 'vendor/autoload.php';
+ 
+$params = array();
+//$params['id'] = 1;
+$params['body']  = array(
+  'name' => '',
+  'age' => 11,
+  'badges' => 8 
+);
 
-$client = new Elasticsearch\Client();
-        
-        print_r($client);
+$params['index'] = 'pokemon';
+$params['type']  = 'pokemon_trainer';
+
+$result = $this->connection()->create($params);
+        echo '<pre>';
+        print_r($result);
        // return $this->render('index');
       //  echo $this->actionSay();
     }
     public function actionSay()
 {
-    return $this->render('say');
+        
+        $params = [
+    'index' => 'pokemon',
+    'type' => 'pokemon_trainer',
+    'body' => [
+        'query' => [
+            'match_all' => []
+        ]
+    ],
+    'client' => [
+        'curl' => [
+            CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1
+        ]
+    ]
+];
+//$selct->search($params);
+        
+      $client =   $this->connection()->search($params);
+ echo '<pre>';
+        print_r($client);
+   // return $this->render('say');
+}
+
+public function connection() {
+    return \Elasticsearch\ClientBuilder::create()->build();
+}
+
+public function actionsSearch(){
+    $model = new \yii\debug\models\search\Log();
+    echo '<pre>';
+        print_r($model);
+        exit;
+        //return $this->render('say');
 }
 }
