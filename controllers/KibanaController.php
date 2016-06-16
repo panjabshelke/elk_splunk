4b172882fbb5b856d1148edb3275bfd6761eb1a0
Bhalchandra <panjabrao@benchmarkitsolutions.com>
2016-06-16 11:02:19 +0530

    kibana controller

--- controllers/KibanaController.php
+++ controllers/KibanaController.php
@@ -46,6 +46,56 @@ class KibanaController extends Controller {
     }
 
     public function actionIndex() {
+        $a = 0;
+        $b = 1;
+        $odd_no = 9;
+        $value = $odd_no / 2;
+
+        for ($i = 1; $i <= $odd_no; $i++) :
+            if ($i % 2 != 0):
+                $temp[] = $i;
+            endif;
+        endfor;
+        foreach ($temp as $key => $val):
+            $temp_var = $odd_no - $val;
+            $left = $right = $temp_var / 2;
+            $left_str = 1;
+            for ($i = 1; $i <= $odd_no; $i++) :
+                if ($left == 0 && $left_str != 0) {
+                    if($val <= 0) {
+                        echo "O ";
+                    } else {
+                        echo "X ";
+                        $val--;
+                    }
+                    $left_str = 1;
+                } else {
+                    if($left!=0){
+                        echo "O ";
+                        $left--;
+                    }
+                }
+            endfor;
+            echo "<br/>";
+        endforeach;
+
+        echo "<br/><br/>End herer<br/><br/>";
+        die;
+//        $curl = new \Elasticsearch\Common\Exceptions\Curl();
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+        $cmd_output = exec('curl -XPUT "localhost:9200/customer?pretty"');
+        print_r($cmd_output);
+
+        for ($i = 1; $i <= 7; $i++) :
+            for ($j = 1; $j <= 7; $j++) :
+                echo "X";
+            endfor;
+            echo "<br/>";
+        endfor;
+
+        echo "heelo";
+        die();
         return $this->render('index');
     }
 
