82f5763cac8accb3d4e1332d6ec48e8b6c55aae7
sachin kudtarkar <sachinkudtarkar07@gmail.com>
2016-06-16 10:53:57 +0530

    update monolog file uploaded

--- modules/elastic/controllers/DefaultController.php
+++ modules/elastic/controllers/DefaultController.php
@@ -6,9 +6,9 @@ use yii;
 use yii\web\Controller;
 use app\modules\Elastic\models\Logs;
 use yii\elasticsearch;
-use index0h\log;
-use index0h\log\base\EmergencyTrait;
-use index0h\log\base\TargetTrait;
+use Monolog\Logger;
+use Monolog\Handler\LogglyHandler;
+use Monolog\Formatter\LogglyFormatter;
 //require 'app/vendor/autoload.php';
 /**
  * Default controller for the `elastic` module
@@ -79,19 +79,32 @@ public function connection() {
 }
 
 public function actionSearch(){
-   $data = new log\LogstashTarget();
-//    $model = new Logs();  
-//      $dataprovider = $model->search('1');
+  
+}
+public function actionMonolog(){    
+echo'<pre>';
+    $logEnv = getenv('1');
+    $level = empty($logLevel)?$logEnv:\Monolog\Logger::WARNING;
+    $log = new Logger('service_log');
+    
+    $strHandler = new \Monolog\Handler\StreamHandler('C:\Users\SACHIN\Desktop\ELK\runtime.log', \Monolog\Logger::DEBUG);
+    //$strHandler = new \Monolog\Handler\SyslogHandler('C:\Users\SACHIN\Desktop\ELK\syslog.txt', \Monolog\Logger::DEBUG);
+    //$fcHandler = new \Monolog\Handler\FingersCrossedHandler($strHandler,$level);
+    
+    $formatter = new \Monolog\Formatter\LogstashFormatter("my Logstash",'application');
+    $strHandler->setFormatter($formatter);
+    $data = $log->pushHandler($strHandler);
+    
+    
+   //$id = $_SERVER['X_VARNISH'];
    
-//      $messages = "warnning";
-//      $final = 1;
-//   $result = $data->collect($messages, $final);
-      print_r($data);
-      die();
-   //$data->collect($messages, $final);
-   
-   
-        //return $this->render('say');
+  // print_r($_SERVER);exit;
+//   $tag = new \Monolog\Processor\TagProcessor(['requested id'=>$id]);
+//   $data =  $log->pushProcessor($tag);
+   $report = $log->debug("logging!");
+
+print_r($strHandler);exit;
+
 }
 
 }
